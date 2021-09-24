58b61768ced2137f800c0e4486ad150d765a4109
yhdchoi <yhdchoi@gmail.com>
2021-09-24 13:48:49 +0900

    Controller&ServiceAdded
    Basic methods created

--- src/main/java/com/yhdc/apiServer/controller/ItemController.java
+++ src/main/java/com/yhdc/apiServer/controller/ItemController.java
@@ -1,19 +1,56 @@
 package com.yhdc.apiServer.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.yhdc.apiServer.model.Item;
+import com.yhdc.apiServer.service.ItemService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @RestController
 public class ItemController {
-	
-	@GetMapping("/")
-	public ResponseEntity<?> findAll(){
-		return new ResponseEntity<String>("ok", HttpStatus.OK);
+
+	private final ItemService itemService;
+
+	@PostMapping("/item/save")
+	public ResponseEntity<Item> saveItem(@RequestBody Item item) {
+		Item result = itemService.saveItem(item);
+		return new ResponseEntity<>(result, HttpStatus.CREATED);
 	}
-	
-	
-	
-	
+
+	@GetMapping("/item/{id}")
+	public ResponseEntity<Item> getItem(@PathVariable Long id) {
+		Item result = itemService.getItem(id);
+		return new ResponseEntity<>(result, HttpStatus.OK);
+	}
+
+	@GetMapping("/items")
+	public ResponseEntity<List<Item>> findAll() {
+		List<Item> result = itemService.findAllItems();
+		return new ResponseEntity<>(result, HttpStatus.OK);
+	}
+
+	@PutMapping("/item/update/{id}")
+	public ResponseEntity<Item> updateItem(@PathVariable Long id, @RequestBody Item item) {
+		Item result = itemService.updateItem(id, item);
+		return new ResponseEntity<>(result, HttpStatus.OK);
+	}
+
+	@DeleteMapping("/item/delete/{id}")
+	public ResponseEntity<String> deleteItem(@PathVariable Long id) {
+		String result = itemService.deleteItem(id);
+		return new ResponseEntity<String>(result, HttpStatus.ACCEPTED);
+	}
+
 }
--- src/main/java/com/yhdc/apiServer/service/ItemService.java
+++ src/main/java/com/yhdc/apiServer/service/ItemService.java
@@ -1,9 +1,50 @@
 package com.yhdc.apiServer.service;
 
-import org.springframework.stereotype.Service;
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.yhdc.apiServer.model.Item;
+import com.yhdc.apiServer.repository.ItemRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Service
 public class ItemService {
 
+	private final ItemRepository itemRepository;
+
+	@Transactional
+	public Item saveItem(Item item) {
+		return itemRepository.save(item);
+	}
+
+	@Transactional(readOnly = true)
+	public Item getItem(Long id) {
+		return itemRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Please check the ID."));
+	}
+
+	@Transactional(readOnly = true)
+	public List<Item> findAllItems() {
+		return itemRepository.findAll();
+	}
 	
+	@Transactional
+	public Item updateItem(Long id, Item item) {
+		// Dirty Check
+		Item itemEntity = itemRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Please check the ID."));
+		
+		itemEntity.setName(item.getName());
+		itemEntity.setContent(item.getContent());
+		
+		return itemEntity;
+	}
+
+	@Transactional
+	public String deleteItem(Long id) {		
+		itemRepository.deleteById(id);
+		return "OK";
+	}
 }
